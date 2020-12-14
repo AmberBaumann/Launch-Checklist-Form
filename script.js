49c1cec3b46c3861f606154fc90434d525499d32
Amber Baumann <ambermindsolliving@gmail.com>
2020-12-13 19:53:53 -0600

    linked!

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 // Write your JavaScript code here!
+function loaded() {
+   let validForm = document.getElementById("launchForm");
+   validForm.addEventListener('submit', function (event) {
+      let pilotInput = document.querySelector("input[name=pilotName]");
+      let coPilotInput = document.querySelector("input[name=copilotName]");
+      let fuelInput = document.querySelector("input[name=fuelLevel]");
+      let cargoInput = document.querySelector("input[name=cargoMass]");
+      if (pilotInput.value === "" || coPilotInput === "") {
+         alert("All fields are required!")
+         event.preventDefault();
+      };
+   });
+};
+window.onload = loaded();
+
+
+
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
