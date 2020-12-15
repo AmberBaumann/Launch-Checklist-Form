dbd7e88f1ec6a1771580998555f4c6001498cf2f
Amber Baumann <ambermindsolliving@gmail.com>
2020-12-14 19:51:09 -0600

    added updating shuttle requirements

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 function loaded() {
    let validForm = document.getElementById("launchForm");
    validForm.addEventListener('submit', function (event) {
+      //VALIDATION
       let pilotInput = document.querySelector("input[name=pilotName]");
       let coPilotInput = document.querySelector("input[name=copilotName]");
       let fuelInput = document.querySelector("input[name=fuelLevel]");
@@ -14,12 +15,33 @@ function loaded() {
       if (pilotInput.value === "" || coPilotInput.value === "" || fuelInput.value === "" || cargoInput.value === "") {
          alert("All fields are required!")
          event.preventDefault();
-      };
-
-      if (!letters.test(pilotInput.value) || !letters.test(coPilotInput.value) || !numbers.test(fuelInput.value) || !numbers.test(cargoInput.value)) {
+      } else if (!letters.test(pilotInput.value) || !letters.test(coPilotInput.value) || !numbers.test(fuelInput.value) || !numbers.test(cargoInput.value)) {
          alert("Invalid input!")
          event.preventDefault();
+      };
+      //UPDATING SHUTTLE REQUIREMENTS
+      let statusUpdate = function () {
+         faultyItems.style.visibility = "visible";
+         pilotStatus.innerText = `Pilot ${pilotName.value} is ready for launch.`;
+         copilotStatus.innerText = `Co-pilot ${copilotName.value} is ready for launch.`;
+
+         if (Number(fuelLevel.value) < 10000) {
+            fuelStatus.innerText = `Fuel level too low for launch.`;
+            launchStatus.style.color = `red`;
+            launchStatus.innerText = `Shuttle not ready for launch`;
+         } else if (Number(cargoMass.value) > 10000) {
+            cargoStatus.innerText = `Cargo mass too heavy for launch`;
+            launchStatus.style.color = `red`;
+            launchStatus.innerText = `Shuttle not ready for launch`;
+         } else {
+            launchStatus.style.color = `green`;
+            launchStatus.innerText = `Shuttle is ready for launch`;
+         }
       }
+
+
+
+
    });
 };
 window.onload = loaded();
