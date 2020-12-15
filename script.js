930ab823ad16f774c8c105de479af29df9d35875
Amber Baumann <ambermindsolliving@gmail.com>
2020-12-14 20:21:11 -0600

    added shuttle requirements

--- script.js
+++ script.js
@@ -1,50 +1,47 @@
 // Write your JavaScript code here!
+let pilotInput = document.querySelector("input[name=pilotName]");
+let coPilotInput = document.querySelector("input[name=copilotName]");
+let fuelInput = document.querySelector("input[name=fuelLevel]");
+let cargoInput = document.querySelector("input[name=cargoMass]");
+
 function loaded() {
    let validForm = document.getElementById("launchForm");
    validForm.addEventListener('submit', function (event) {
       //VALIDATION
-      let pilotInput = document.querySelector("input[name=pilotName]");
-      let coPilotInput = document.querySelector("input[name=copilotName]");
-      let fuelInput = document.querySelector("input[name=fuelLevel]");
-      let cargoInput = document.querySelector("input[name=cargoMass]");
-
       const letters = /^[a-zA-Z]+$/;
       const numbers = /^[0-9]+$/;
 
 
       if (pilotInput.value === "" || coPilotInput.value === "" || fuelInput.value === "" || cargoInput.value === "") {
          alert("All fields are required!")
-         event.preventDefault();
       } else if (!letters.test(pilotInput.value) || !letters.test(coPilotInput.value) || !numbers.test(fuelInput.value) || !numbers.test(cargoInput.value)) {
          alert("Invalid input!")
-         event.preventDefault();
       };
-      //UPDATING SHUTTLE REQUIREMENTS
-      let statusUpdate = function () {
-         faultyItems.style.visibility = "visible";
-         pilotStatus.innerText = `Pilot ${pilotName.value} is ready for launch.`;
-         copilotStatus.innerText = `Co-pilot ${copilotName.value} is ready for launch.`;
-
-         if (Number(fuelLevel.value) < 10000) {
-            fuelStatus.innerText = `Fuel level too low for launch.`;
-            launchStatus.style.color = `red`;
-            launchStatus.innerText = `Shuttle not ready for launch`;
-         } else if (Number(cargoMass.value) > 10000) {
-            cargoStatus.innerText = `Cargo mass too heavy for launch`;
-            launchStatus.style.color = `red`;
-            launchStatus.innerText = `Shuttle not ready for launch`;
-         } else {
-            launchStatus.style.color = `green`;
-            launchStatus.innerText = `Shuttle is ready for launch`;
-         }
-      }
-
-
-
-
+      statusUpdate();
+      event.preventDefault();
    });
+
+   //UPDATING SHUTTLE REQUIREMENTS
+   let statusUpdate = function () {
+      faultyItems.style.visibility = 'visible';
+      pilotStatus.innerText = `Pilot ${pilotInput.value} is ready for launch.`;
+      copilotStatus.innerText = `Co-pilot ${coPilotInput.value} is ready for launch.`;
+
+      if (Number(fuelInput.value) < 10000) {
+         fuelStatus.innerText = `Fuel level too low for launch.`;
+         launchStatus.style.color = `red`;
+         launchStatus.innerText = `Shuttle not ready for launch`;
+      } else if (Number(cargoInput.value) > 10000) {
+         cargoStatus.innerText = `Cargo mass too heavy for launch`;
+         launchStatus.style.color = `red`;
+         launchStatus.innerText = `Shuttle not ready for launch`;
+      } else {
+         launchStatus.style.color = `green`;
+         launchStatus.innerText = `Shuttle is ready for launch`;
+      }
+   }
 };
-window.onload = loaded();
+window.onload = loaded;
 
 
 
