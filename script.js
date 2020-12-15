a546498ab26b0a8504043821c5f67f62c66d1437
Amber Baumann <ambermindsolliving@gmail.com>
2020-12-14 20:44:20 -0600

    finished fetching planetary data

--- script.js
+++ script.js
@@ -40,6 +40,26 @@ function loaded() {
          launchStatus.innerText = `Shuttle is ready for launch`;
       }
    }
+   // FETCHING PLANETARY DATA
+   let planetaryData = function () {
+      fetch("https://handlers.education.launchcode.org/static/planets.json").then(function (response) {
+         response.json().then(function (json) {
+            missionTarget.innerHTML = `
+         <h2>Mission Destination</h2>
+         <ol>
+            <li>Name: ${json[0].name}</li>
+            <li>Diameter: ${json[0].diameter}</li>
+            <li>Star: ${json[0].star}</li>
+            <li>Distance from Earth: ${json[0].distance}</li>
+            <li>Number of Moons: ${json[0].moons}</li>
+         </ol>
+         <img src = "${json[0].image}">
+         `;
+         });
+      });
+   }
+   planetaryData();
+
 };
 window.onload = loaded;
 
